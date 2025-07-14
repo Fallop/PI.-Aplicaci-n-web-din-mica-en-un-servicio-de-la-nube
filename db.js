790064b08da424bd1626145f9b05239db8c62299
FM <152152436+Fallop@users.noreply.github.com>
2025-07-14 10:16:36 -0600

    Peliculas v1.1

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db/database.sqlite');
+const db = new sqlite3.Database('database.sqlite');
 
 // Crear tabla si no existe
 db.serialize(() => {
